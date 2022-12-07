020e78f890efa9c5468f9531d1d371ea8659458f
lilian <lilian.wangechi@student.moringaschool.com>
2022-12-07 10:17:58 +0300

    new app,models, controllers

---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  belongs_to :plants
+  belongs_to :user
+
+  validates :rating, numericality: { less_than_or_equal_to: 5 }
+end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_many :plants, through: :reviews
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+
+  
+end
