fdc5b46490d64afb3b566403b4534ddbcdfa3d9d
lilian <lilian.wangechi@student.moringaschool.com>
2022-12-07 13:07:07 +0300

    Add session  controler.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def login
+    user = User.find_by(username: params[:username])
+    if user&.authenticate(params[:password])
+      session[:current_user] = user.id
+      render json: user, status: :ok
+    else
+      render json: { error: ["invalid username and/or password"] }, status: :unauthorized
+    end
+  end
+
+  def logout
+    session.delete :current_user
+  end
+
+end
